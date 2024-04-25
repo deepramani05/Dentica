4623eba1967340575bee3c00faeeafcd558c8797
Yashpal-vaghela <vaghela9632@gmail.com>
2024-04-25 19:21:15 +0530

    api added

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IoIosMail } from "react-icons/io";
 import { FaPhone } from "react-icons/fa6";
 import { FaBook } from "react-icons/fa";
 import { FaTextWidth } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 
 const Career = () => {
+  const [formData, setFormData]= useState({
+    name:"",
+    email:"",
+    mobile_number:"",
+    subject:"",
+    file:""
+  });
+
+  const handleChange = (e) =>{
+    if (e.target.name === "file"){
+      setFormData({ ...formData, file: e.target.files[0] });
+    }else {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      const formDataToSend = new FormData();
+      formDataToSend.append("name",formData.name);
+      formDataToSend.append("email",formData.email);
+      formDataToSend.append("mobile",formData.mobile_number);
+      formDataToSend.append("subject",formData.subject);
+      formDataToSend.append("file",formData.file);
+
+      const response = await fetch("https://denticadentalstudio.com/api/career",{
+        method: "POST",
+        body: formDataToSend
+      });
+
+      if (response.ok){
+        console.log("Application submitted successfully");
+      }else{
+        console.log("Failed to submit application");
+      }
+    }catch (error){
+      console.error("Network error:", error);
+    }
+  };
   return (
     <div className="career-main">
       <div className="career-sub">
@@ -34,12 +74,18 @@ const Career = () => {
                 </p>
               </div>
               <div className="career-form-main">
-                <form className="home-msg-form">
+                <form className="home-msg-form" onSubmit={handleSubmit}>
                   <div className="home-msg-form-p1">
                     <div className="home-msg-input-div">
                       <p>Name</p>
                       <div>
-                        <input type="text" placeholder="Name" />
+                        <input 
+                          type="text" 
+                          placeholder="Name"
+                          name="name" 
+                          value={formData.name} 
+                          onChange={handleChange}
+                        />
                         <span>
                           <FaUser />
                         </span>
@@ -48,7 +94,13 @@ const Career = () => {
                     <div className="home-msg-input-div">
                       <p>Email</p>
                       <div>
-                        <input type="text" placeholder="Email" />
+                        <input 
+                          type="text" 
+                          placeholder="Email" 
+                          name="email"
+                          value={formData.email}
+                          onChange={handleChange}
+                        />
                         <span>
                           <IoIosMail />
                         </span>
@@ -59,7 +111,13 @@ const Career = () => {
                     <div className="home-msg-input-div">
                       <p>Phone</p>
                       <div>
-                        <input type="text" placeholder="Phone" />
+                        <input 
+                          type="text" 
+                          placeholder="Phone"
+                          name="mobile_number"
+                          value={formData.mobile_number}
+                          onChange={handleChange}
+                          />
                         <span>
                           <FaPhone />
                         </span>
@@ -68,7 +126,12 @@ const Career = () => {
                     <div className="home-msg-input-div">
                       <p>Subject</p>
                       <div>
-                        <input type="text" placeholder="Apply for" />
+                        <input 
+                          type="text" 
+                          placeholder="Apply for"
+                          name="subject" 
+                          onChange={handleChange}
+                        />
                         <span>
                           <FaBook />
                         </span>
