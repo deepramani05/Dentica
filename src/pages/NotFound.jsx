fcac6ce27df4e46c47d7e65f37092809a74e2be1
deepramani05 <135039433+deepramani05@users.noreply.github.com>
2024-05-22 12:21:27 +0530

    NotFound page style

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="not-found-main">
       <div className="not-found-container">
         <h1 className="not-found-title">404 - Not Found</h1>
         <p className="not-found-text">
